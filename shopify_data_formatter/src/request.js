7e2856ef19624cc9f5436543b55583b6249faafa
digital-autumn <software.engineer.hub@protonmail.com>
2025-02-15 15:14:32 -0500

    Update request.js

--- shopify_data_formatter/src/request.js
+++ shopify_data_formatter/src/request.js
@@ -5,22 +5,21 @@
  * @returns data in JSON format retrived from the wholesaler
  */
 export const getData = async (url, uName, uKey) => {
-  
-  const auth = 'Basic '+btoa(`${uName}:${uKey}`);
+
+  const options = { 
+    method: 'get',
+    headers: {
+      Authorization: `Basic ${btoa(`${uName}:${uKey}`)}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    mode: 'cors',
+    cache: 'default'
+  };
   
   try {
-    const response = await fetch(url, 
-      { 
-        method: 'get',
-        headers: {
-          'Authorization': auth,
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        mode: 'cors',
-        cache: 'default'
-      });
-      
-      return await response.json();
+    const response = await fetch(url, options);
+    return await response.json();
 
   } catch (error) {
     console.error(error);
