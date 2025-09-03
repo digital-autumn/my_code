e6ee8cfb6770dcbc305c4893d51125d2049417f7
digital-autumn <software.engineer.hub@protonmail.com>
2025-09-03 13:49:33 -0400

    Updated scraper.js
    Updated scraper.js

--- web_scraper/scraper.js
+++ web_scraper/scraper.js
@@ -1,44 +1,43 @@
-import * as cheerio from 'cheerio';
-import * as converter from 'json-2-csv';
+import { load } from 'cheerio';
+import { json2csv } from 'json-2-csv';
 import fs from 'fs';
 
 const priceFormatter = (str) => {return str.substring(str.length-8, str.length).replace('^0+', '');};
+var numProds = 0;
 
 export const getData = async (url) => {
 
    const data = [];
-   let numProds = 0;
-
   try {
 
    const response = await fetch(url);
    const body = await response.text();
 
-   const $ = cheerio.load(body);
+   const $ = load(body);
    const products = $('li.product_wrapper');
 
-   products.each((i, e) => {
+   products.map((e) => {
 
       const model = $(e).find('div.h2').text().replace('\n', ' ').trim().split(';')[0];
       const sku = $(e).find('.sku').text().trim().replace(/[^\d.-]/g, '');
       const price = priceFormatter($(e).find('div.price').text().trim()).replace(/[^\d.-]/g, '');
       
       data.push({
-         model,
-         sku,
-         price
+         "Model": model,
+         "SKU": sku,
+         "Price": price
       });
       
       numProds++;
    });
    
    fs.appendFile('e_commerce_data.json', JSON.stringify(data), (err) => {if (err) throw err;});
-
-   fs.appendFile('e_commerce_data.csv', converter.json2csv(data), (err) => {if (err) throw err;});
-
-   console.log(numProds);
+   fs.appendFile('e_commerce_data.csv', json2csv(data), (err) => {if (err) throw err;});
   }
+
   catch(err){
    console.error(err);
   }
-};
+}
+
+
